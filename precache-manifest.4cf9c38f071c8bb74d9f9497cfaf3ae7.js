2e5537b5a43e46990a61f7c6cbb4f26a9f534047
Splendid-Satyam <58887621+Splendid-Satyam@users.noreply.github.com>
2020-06-19 13:42:21 +0530

    Updates

---
+++ precache-manifest.4cf9c38f071c8bb74d9f9497cfaf3ae7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71a73188465a9d0de64bcd177d4883ed",
+    "url": "/splendid-face-recognition/index.html"
+  },
+  {
+    "revision": "8f9ca086a31b993359f4",
+    "url": "/splendid-face-recognition/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "0bd832850a605cf92937",
+    "url": "/splendid-face-recognition/static/css/main.c218bd93.chunk.css"
+  },
+  {
+    "revision": "8f9ca086a31b993359f4",
+    "url": "/splendid-face-recognition/static/js/2.de4f281e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/splendid-face-recognition/static/js/2.de4f281e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bd832850a605cf92937",
+    "url": "/splendid-face-recognition/static/js/main.a1c91c20.chunk.js"
+  },
+  {
+    "revision": "5d8d4a71a07ebf70ada5",
+    "url": "/splendid-face-recognition/static/js/runtime-main.53a485db.js"
+  }
+]);
